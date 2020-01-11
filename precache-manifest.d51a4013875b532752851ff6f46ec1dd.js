f6303dfcc1fa7b005832dfb4ea95b412d6fb3e07
Marko <markotopitsch@gmail.com>
2020-01-11 09:27:45 -0500

    Updates

---
+++ precache-manifest.d51a4013875b532752851ff6f46ec1dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c76e4fdfcca6643d8d017d65ac1d7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd0ad6f1582bbf3fd0b3",
+    "url": "/static/css/main.4faa341a.chunk.css"
+  },
+  {
+    "revision": "5134d006c95e5be6f28e",
+    "url": "/static/js/2.8f612c6e.chunk.js"
+  },
+  {
+    "revision": "fd0ad6f1582bbf3fd0b3",
+    "url": "/static/js/main.5dccfee1.chunk.js"
+  },
+  {
+    "revision": "5deaf508f7a02a0b3be3",
+    "url": "/static/js/runtime-main.cf97cbd6.js"
+  }
+]);
